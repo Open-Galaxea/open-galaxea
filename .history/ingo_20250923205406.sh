4d439810bdbe9521fd782bf836bbba9a659e29a9
Bailey-24 <2436442868@qq.com>
2025-09-23 21:06:57 +0800

    remove .history from git tracking

--- .history/ingo_20250923205406.sh
+++
@@ -1,7 +0,0 @@
-# 遍历每个子目录的 .gitignore
-for dir in G0 GalaxeaDP GalaxeaLeRobot GalaxeaManipSim; do
-  if [ -f "$dir/.gitignore" ]; then
-    # 给每行规则加上前缀
-    sed "s|^|$dir/|" "$dir/.gitignore" >> .gitignore
-  fi
-done
